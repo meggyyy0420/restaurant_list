57b59cb657ae31005dbb20acd6645446c458f78f
amberrr0420 <amberrr0420@gmail.com>
2021-09-27 15:55:33 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -18,32 +18,18 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-
 const restaurantList = require('./models/seeds/restaurant.json')
 
-// // setting template engine
-// app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-// app.set('view engine', 'handlebars')
+// setting template engine
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
-// app.use(express.static('public'))
+app.use(express.static('public'))
 
-// // routes setting
-// app.get('/', (req, res) => {
-//   res.render('index', { restaurants: restaurantList.results })
-// })
-
-// app.get('/search', (req, res) => {
-//   const keyword = req.query.keyword
-//   const restaurants = restaurantList.results.filter(restaurant => {
-//     return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
-//   })
-//   res.render('index', { restaurants: restaurants, keyword: keyword });
-// })
-
-// app.get('/restaurants/:restaurant_id', (req, res) => {
-//   const restaurant = restaurantList.results.filter(restaurant => restaurant.id === Number(req.params.restaurant_id))
-//   res.render('show', { restaurant: restaurant[0] })
-// })
+// routes setting
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
 
 // start and listen on the Express server
 app.listen(port, () => {
