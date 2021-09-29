8531115a6f87b3e7a42d3f23a49cbfb9dff8e098
amberrr0420 <amberrr0420@gmail.com>
2021-09-29 20:05:14 +0800

    feat: add create restaurant function

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const express = require('express');
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const Restaurant = require('./models/restaurant')
+const bodyParser = require('body-parser')
 const app = express();
 const port = 3000;
 
-const Restaurant = require('./models/restaurant')
+
 
 mongoose.connect('mongodb://localhost/restaurant-list')
 const db = mongoose.connection
@@ -24,12 +26,67 @@ const restaurantList = require('./models/seeds/restaurant.json')
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+// setting static files ///
 app.use(express.static('public'))
+
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(express.urlencoded({ extended: true }))
 
 // routes setting
 app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurantList.results })
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(err => console.log(err))
 })
+
+app.get('/restaurants/new', (req, res) => {
+  res.render('new')
+})
+
+app.post('/restaurants', (req, res) => {
+  return Restaurant.create(req.body)
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+})
+
+// app.get('/todos/:id', (req, res) => {
+//   const id = req.params.id
+//   Todo.findById(id)
+//     .lean()
+//     .then(todo => res.render('detail', { todo }))
+//     .catch(err => console.log(err))
+
+// })
+
+// app.get('/todos/:id/edit', (req, res) => {
+//   const id = req.params.id
+//   Todo.findById(id)
+//     .lean()
+//     .then(todo => res.render('edit', { todo }))
+//     .catch(err => console.log(err))
+
+// })
+
+// app.post('/todos/:id/edit', (req, res) => {
+//   const id = req.params.id
+//   const name = req.body.name
+//   Todo.findById(id)
+//     .then(todo => {
+//       todo.name = name
+//       return todo.save()
+//     })
+//     .then(() => res.redirect(`/todos/${id}`))
+//     .catch(error => console.log(error))
+// })
+
+// app.post('/todos/:id/delete', (req, res) => {
+//   const id = req.params.id
+//   return Todo.findById(id)
+//     .then(todo => todo.remove())
+//     .then(() => res.redirect('/'))
+//     .catch(error => console.log(error))
+// })
 
 // start and listen on the Express server
 app.listen(port, () => {
