42176abe9a8f27a72d92e768c3f50777c8a32012
amberrr0420 <amberrr0420@gmail.com>
2021-10-02 09:09:01 +0800

    feat: renew score required in new.hbs andchange router(put) in restanrant.js

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -33,28 +33,14 @@ router.get('/:id/edit', (req, res) => {
 
 router.put('/:id', (req, res) => {
   const id = req.params.id
-  const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
-  Restaurant.findById(id)
-    .then(restaurant => {
-      restaurant.name = name
-      restaurant.name_en = name_en
-      restaurant.category = category
-      restaurant.image = image
-      restaurant.location = location
-      restaurant.phone = phone
-      restaurant.google_map = google_map
-      restaurant.rating = rating
-      restaurant.description = description
-      return restaurant.save()
-    })
+  Restaurant.findByIdAndUpdate(id, { $set: req.body })
     .then(() => res.redirect(`/restaurants/${id}`))
     .catch(error => console.log(error))
 })
 
 router.delete('/:id', (req, res) => {
   const id = req.params.id
-  return Restaurant.findById(id)
-    .then(restaurant => restaurant.remove())
+  return Restaurant.findByIdAndRemove(id)
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
